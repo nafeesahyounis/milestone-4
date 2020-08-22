292291db2a1bd78ad977e8edb603d37229b8fab1
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-08-22 15:31:53 +0000

    added checkout app to settings

--- checkout/views.py
+++ checkout/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def test(request):
+def checkouttest(request):
     """A view to return the checkouttest page"""
     return render(request, 'checkout/checkouttest.html')
