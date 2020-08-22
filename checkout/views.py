0abb61119ee20be142e7ce98cc1754f1b44add2f
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-08-22 15:31:06 +0000

    added view for checkouttest test template for checkout app in checkout app folder

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def test(request):
+    """A view to return the checkouttest page"""
+    return render(request, 'checkout/checkouttest.html')
