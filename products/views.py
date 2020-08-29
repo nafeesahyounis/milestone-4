03e86521bcaa9302be4711ae45c4ab3589f4c9d2
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-08-29 16:15:33 +0000

    rendered test product on test template

--- products/views.py
+++ products/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 def test(request):
-    """A view to return the index page"""
-    return render(request, 'products/test.html')
+    """A view to return the test page"""
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/test.html', context)
