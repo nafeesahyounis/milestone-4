92a9021b058ef83eb7de31fabbc5ef91fba4ddcd
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-10-05 16:18:57 +0000

    deployment part 1 to heroku

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -104,11 +105,15 @@ WSGI_APPLICATION = 'milestone4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse('postgres://gceevkdfbxicye:f22799f17d876a52829ba7777e6c3f020b7261b9e884cffd310a381125de77b6@ec2-52-21-0-111.compute-1.amazonaws.com:5432/df76r1rt87amhi')
 }
 
 
