704ffb9c401533f3dee49bda995d5334af8427f9
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-10-14 14:50:34 +0000

    created a view that takes the items in the checkout cart in admin and renders onto checkout page

--- checkout/models.py
+++ checkout/models.py
@@ -3,13 +3,11 @@ from products.models import Product
 
 # Create your models here.
 
-class Cart(models.Model):
 
-    product = models.ManyToManyField(Product, null=True, blank=True),
+class Cart(models.Model):
+    products = models.ManyToManyField(Product)
     total = models.DecimalField(max_digits=100, decimal_places=2, default=0.00)
 
-    def __str__(self):
-        return self.name
 
     def __unicode__(self):
         return "Cart if %s" % (self.id)
--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Cart
 
 # Create your views here.
 
 def checkouttest(request):
     """A view to return the checkouttest page"""
-    return render(request, 'checkout/checkouttest.html')
+    cart = Cart.objects.all()[0]
+    context = {'cart': cart}
+    return render(request, 'checkout/checkouttest.html', context)
