415167f6d5c636e6bc286ebb69e8c6e4377dfded
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-10-14 10:47:18 +0000

    created model for cart in checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from products.models import Product
 
 # Create your models here.
+
+class Cart(models.Model):
+
+    product = models.ManyToManyField(Product, null=True, blank=True),
+    total = models.DecimalField(max_digits=100, decimal_places=2, default=0.00),
+
+    def __unicode__(self):
+        return "Cart if %s" % (self.id)
