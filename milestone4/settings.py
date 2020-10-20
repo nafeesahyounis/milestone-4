f1a863d6fa43027c47422cc862b7edbc695be416
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-10-20 13:51:53 +0000

    added database url into gitpod variables

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+DATABASE_URL = os.environ['DATABASE_URL'] 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
--- milestone4/urls.py
+++ milestone4/urls.py
@@ -10,7 +10,4 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('checkout/', include('checkout.urls')),
     path('cart/', include('cart.urls')),
-
-
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
