68ae1fb13d38a3174da6154e2eaaf650aa62d4f1
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-10-21 10:42:56 +0000

    if statement is being skipped in views.py so testing

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render,redirect,reverse, get_object_or_404
 from .models import OrderItem
 from .forms import OrderCreateForm
-from cart.contexts import cart_items
+from products.models import Product
 
 # Create your views here.
 
@@ -14,29 +14,49 @@ def checkouttest(request):
 def create_order(request):
 
     cart = request.session.get('cart', {})
-    print('these are the', cart_items)
 
     print('cart printed', cart)
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
         if form.is_valid():
             order = form.save()
-            for item in cart:
-                new_item = OrderItem.objects.create(order=order,
-                                                    product=item['product'],
-                                                    price=item['price'],
-                                                    quantity=item['quantity'])
-                print('new item is', new_item)
-            # clear the cart
-            cart.clear()
-            return render(request,
-                          'checkout/order_created.html',
-                          {'order': order})
+            for key, value in cart.items():
+                try:
+                    product = Product.objects.get(id=key)
+                    print('product:', product)
+                    if isinstance(value, int):
+                        order_line_item = OrderItem(
+                            order=order,
+                            product=product,
+                            quantity=value,
+                        )
+                        order_line_item.save()
+                        print('saved', order_line_item)
+                    else:
+                        print('we are jumping')
+
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your bag wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('checkout/order_created.html'))
     else:
         form = OrderCreateForm()
-    return render(request,
-                  'checkout/order_created.html',
-                  {'cart': cart, 'form': form})
+        (print, 'else clause')
+        return render(request, 'checkout/order_created.html', {'cart': cart, 'form': form})
+                    
+            # clear the cart
+            #cart.clear()
+            #return render(request,
+            #              'checkout/order_created.html',
+            #              {'order': order})
+   # else:
+   #     form = OrderCreateForm()
+   # return render(request,
+   #               'checkout/order_created.html',
+   #               {'cart': cart, 'form': form})
 
 
 
