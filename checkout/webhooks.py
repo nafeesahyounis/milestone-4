09e6a824a0a1ac0662fdf77f6c199c303ea58e94
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-10-28 15:09:56 +0000

    getting stripe webhook error so adding wh secret to gitpod variables

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,31 +1,40 @@
 import json
 from django.http import HttpResponse
 from django.conf import settings
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 
-stripe_api_key = settings.STRIPE_SECRET_KEY
+from checkout.webhook_handler import StripeWH_Handler
 
+
+
+@require_POST
+@csrf_exempt
 # Using Django
-def my_webhook_view(request):
-  payload = request.body
-  event = None
+def webhook(request):
 
-  try:
-    event = stripe.Event.construct_from(
-      json.loads(payload), stripe.api_key
-    )
-  except ValueError as e:
-    # Invalid payload
-    return HttpResponse(status=400)
+    #setup
+    stripe_api_key = settings.STRIPE_SECRET_KEY
+    wh_secret = settings.STRIPE_WH_SECRET
+    payload = request.body
+    event = None
 
-  # Handle the event
-  if event.type == 'payment_intent.succeeded':
-    payment_intent = event.data.object # contains a stripe.PaymentIntent
-    print('PaymentIntent was successful!')
-  elif event.type == 'payment_method.attached':
-    payment_method = event.data.object # contains a stripe.PaymentMethod
-    print('PaymentMethod was attached to a Customer!')
-  # ... handle other event types
-  else:
-    print('Unhandled event type {}'.format(event.type))
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
 
-  return HttpResponse(status=200)
+    try:
+        event = stripe.Webhook.construct_event(
+        payload, sig_header, wh_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+    except Exception as e:
+        return HttpResponse(content=2, status=400)
+
+    print('success!')
+    return HttpResponse(status=200)
