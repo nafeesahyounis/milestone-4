9d6659f67defa7c387ad1cffc035f3c3351bb4b2
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-10-30 18:26:00 +0000

    testing js functionality in main.js

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+addToCart = document.getElementsByClassName('add-to-cart')
+
+addToCart.addEventListener('click', success)
+
+function success(){
+    console.log("I have been clicked successfully")
+}
